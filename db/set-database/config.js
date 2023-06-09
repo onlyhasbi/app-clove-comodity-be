1d1940cecd6a7de2dbc11e963d10a93b9841af8f
Ahmad Zulal Zaqi <zulal04>
2023-06-09 20:04:10 +0800

    m

--- db/set-database/config.js
+++ db/set-database/config.js
@@ -2,7 +2,7 @@ const con = {
     user: 'postgres',  //?sesuaikan username postgres anda
     host: 'localhost', //?sesuaikan host postgres anda
     database: 'postgres', 
-    password: '<password >',  //?sesuaikan host postgres anda
+    password: '154763',  //?sesuaikan host postgres anda
     port: 5432, //?sesuaikan host postgres anda
 };
 module.exports = con;
